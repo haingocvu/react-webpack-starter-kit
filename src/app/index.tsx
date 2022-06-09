c6a2115ac1e82437a8a150c06b03e07b7f07d720
Hai Vu Ngoc <ngochai.200290@gmail.com>
2022-06-09 16:32:10 +0700

    notfound

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -9,7 +9,7 @@ function App() {
     <>
       <Routes>
         <Route path='/' element={<Home />} />
-        <Route path='abc' element={<NotFound />} />
+        <Route path='*' element={<NotFound />} />
       </Routes>
     </>
   );
