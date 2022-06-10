223819b745dc298fc8511405781de7d6d3cbd60b
Hai Vu Ngoc <ngochai.200290@gmail.com>
2022-06-10 14:20:14 +0700

    add redux toolkit

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,6 +3,7 @@ import { Routes, Route } from 'react-router-dom';
 
 import { NotFoundPage } from './pages/NotFound/Loadable';
 import { HomePage } from './pages/Home/Loadable';
+import { GlobalStyle } from 'styles/global-styles';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
         <Route path='/' element={<HomePage />} />
         <Route path='*' element={<NotFoundPage />} />
       </Routes>
+      <GlobalStyle />
     </>
   );
 }
